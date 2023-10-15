583d7f183dd46b3d8dfd922c869962d4ef346754
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2023-10-15 18:14:32 +0000

    Updated volumeSurfAreaTrapezoidPrism.cpp

--- volumeSurfAreaTrapezoidPrism.cpp
+++ volumeSurfAreaTrapezoidPrism.cpp
@@ -1,34 +1,33 @@
 // Copyright (c) 2023 Julien Lamoureux All rights reserved.
 // .
 // Created by: Julien Lamoureux
-// Date: September 26, 2023
-// Calculating the surface area and volume of trapezoidal prism
+// Date: October 15, 2023
+// Calculating the surface area and volume of a trapezoidal prism
 
-#include <cmath>
 #include <iostream>
 #include <iomanip>
 
 int main() {
-    // declare variables
+    // declare all variables
     float sideA, sideB, sideC, sideD, length, height,
-        surfaceArea, volume, roundedSurfaceArea;
+        surfaceArea, volume;
     std::string units;
 
-    // get the length and width
-    std ::cout << "Area and Perimeter of a Rectangle" << std::endl;
-    std::cout << "How long is side a (the left diagonal)?";
+    // get all the sides of the trapezoidal prism and the units
+    std::cout << "Surface Area and Volume of a Trapezoidal Prism" << std::endl;
+    std::cout << "How long is side a (the left diagonal): ";
     std::cin >> sideA;
-    std::cout << "How long is side b (the long base)?";
+    std::cout << "How long is side b (the base on the bottom): ";
     std::cin >> sideB;
-    std::cout << "How long is side c (the right diagonal)?";
+    std::cout << "How long is side c (the right diagonal): ";
     std::cin >> sideC;
-    std::cout << "How long is side d (the short base)";
+    std::cout << "How long is side d (the base on the top): ";
     std::cin >> sideD;
-    std::cout << "What is the length of the prism?";
+    std::cout << "What is the length of the prism: ";
     std::cin >> length;
-    std::cout << "What is the length of the prism?";
+    std::cout << "What is the height of the prism: ";
     std::cin >> height;
-    std::cout << "What units would you like to be used (ex. cm)?";
+    std::cout << "The units that you would like to be used (ex. cm): ";
     std::cin >> units;
 
     // if any of the sides <= 0, then tell the user to enter a positive number.
@@ -43,7 +42,7 @@ int main() {
     } else if (length <= 0) {
         std ::cout << "Enter a positive number for the length." << std::endl;
     } else if (height <= 0) {
-        std ::cout << "Enter a positive number for the width." << std::endl;
+        std ::cout << "Enter a positive number for the height." << std::endl;
     } else {
         // otherwise, calculate the surface area and volume
         surfaceArea = height * (sideB + sideD) + length
@@ -53,11 +52,11 @@ int main() {
         // display the surface area
         std ::cout << "The surface area is "
             << std::fixed << std::setprecision(2) << std::setfill('0')
-            << surfaceArea << " " << units << "^2" << std::endl;
+            << surfaceArea << " " << units << "^2." << std::endl;
 
         // display the volume
         std ::cout << "The volume is "
             << std::fixed << std::setprecision(2) << std::setfill('0')
-            << volume << " " << units << "^3" << std::endl;
+            << volume << " " << units << "^3." << std::endl;
     }
 }
